e53c86bd7e065f10c1d67f84a018c7446f91366f
ge-wu <jiayewang65@gmail.com>
2021-09-26 21:28:13 -0600

    Add untested kd tree search

--- 05OrthogonalRangeSearching/kd_tree.cpp
+++ 05OrthogonalRangeSearching/kd_tree.cpp
@@ -16,22 +16,25 @@ struct KDNode {
     KDNode(Point pt_): pt(pt_), left(nullptr), right(nullptr) {}
 };
 
+// Sort points by x-axis
 bool by_x(const Point &a, const Point &b) {
     return a.x < b.x;
 }
 
+// Sort points by y-axis
 bool by_y(const Point &a, const Point &b) {
     return a.y < b.y;
 }
 
 KDNode* buildKDTree(vector<Point> P, int depth) {
-    if (!P.size()) 
-        return NULL;
-    if (depth & 1) 
-        sort(P.begin(), P.end(), by_y);
-    else 
-        sort(P.begin(), P.end(), by_x);
+    if (!P.size()) return NULL;
 
+    if (depth & 1) sort(P.begin(), P.end(), by_y); // horizontal split
+    else sort(P.begin(), P.end(), by_x);  // vertical split
+
+    /* The textbook store the line of splitter as internal node, point as 
+     * leaf nodes. This is unnecessary if you think about how the tree is 
+     * built and the property of each node's left and right subtree. */
     int mid = P.size() / 2;
     KDNode *node = new KDNode(P[mid]);
     vector<Point> P1(P.begin(), P.begin() + mid);
@@ -41,8 +44,53 @@ KDNode* buildKDTree(vector<Point> P, int depth) {
     return node;
 }
 
+// R consists of two points which are the diagonal of a rectangle.
+vector<Point> searchKDTree(KDNode* root, pair<Point, Point> R, int depth=0) {
+    int splitValue, min, max;
+    if (!root) 
+        return {};
+    if (depth & 1) {
+        splitValue = root->pt.y;
+        min = R.first.x, max = R.second.x;
+    } else {
+        splitValue = root->pt.x;
+        min = R.first.y, max = R.second.y;
+    }
+
+    if (splitValue < min)
+        return searchKDTree(root->right, R, depth + 1);
+    else if (max < splitValue) 
+        return searchKDTree(root->left, R, depth + 1);
+    else {
+        // in-range elements could exist in both subtree
+        vector<Point> l, r;
+        l = searchKDTree(root->left, R, depth + 1);
+        r = searchKDTree(root->right, R, depth + 1);
+        l.insert(l.end(), r.begin(), r.end());
+        // both coordinate of root are in range
+        if (root->pt.x >= R.first.x && root->pt.x <= R.second.x && 
+                root->pt.y >= R.first.y && root->pt.y <= R.second.y) {
+            l.push_back(root->pt);
+        }
+        return l;
+    }
+}
+
+
 int main() {
-    vector<Point> pts = {{7,2},{5,4},{9,6},{4,7},{8,1},{2,3}};
-    KDNode *root = buildKDTree(pts, 0);
+    vector<Point> pts, ans;
+    pair<Point, Point> R;
+    KDNode *root;
+
+    pts = {{7,2},{5,4},{9,6},{4,7},{8,1},{2,3}};
+    R = {{1,0},{6,5}};
+
+    
+    root = buildKDTree(pts, 0);
+    ans = searchKDTree(root, R);
+
+    for (auto a: ans)
+        cout << a.x << " " << a.y << endl;
+
     printf("Done\n");
 }
